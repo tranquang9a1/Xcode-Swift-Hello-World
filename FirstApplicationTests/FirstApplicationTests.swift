d47c4331ab31f4250b64868e08b3390b498c9d39
Tran Vinh Quang <QuangTV@trans-mac-mini.local>
2015-10-27 17:12:56 +0700

    Test file

--- FirstApplicationTests/FirstApplicationTests.swift
+++ FirstApplicationTests/FirstApplicationTests.swift
@@ -7,30 +7,18 @@
 //
 
 import XCTest
+import UIKit
 @testable import FirstApplication
 
 class FirstApplicationTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testMealInitialization() {
+        let potentialItem = Meal(name: "Newest", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        let badRating = Meal(name: "Bad Rating", photo: nil, rating: -1)
+        XCTAssertNil(badRating)
     }
     
 }
